f531ad8b5feebdcf1a88ab2c5b40057f1ac9f126
SaraNagi98 <saranagi98@gmail.com>
2023-04-26 11:00:14 +0200

    DetectH2

--- Assets/ExperimentScripts_2-b1-3/DetectH2.cs
+++ Assets/ExperimentScripts_2-b1-3/DetectH2.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class DetectH2 : MonoBehaviour
 {
     [System.NonSerialized] public bool IsDetectedH2;
+    [SerializeField] ParticleSystem _particleSystem_Explosion;
+    bool InH2;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (InH2 && DetectSockets.Isbubble) 
+        {
+           
+            _particleSystem_Explosion.Play();
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!InH2 && other.gameObject.tag == "H2" )
+        {
+            InH2 = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (InH2 && other.gameObject.tag == "H2")
+        {
+            InH2 = false;
+        }
     }
 }
